e3fd063c1f61ceeeb965c9875ae7f6de892f86d9
Luan oliveira <luannoliveira1802@gmail.com>
2026-09-19 15:57:27 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+import pandas as pd
 import streamlit as st
 import json
 import requests
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout='wide')
 
-with open('pokemon_index.json', 'r', encoding='utf-8') as arquivo:
+with open('Pokemon_index.json', 'r', encoding='utf-8') as arquivo:
     nomes_pokemons = json.load(arquivo)
 
-nome = st.selectbox('Escolha um Pokemon', nomes_pokemons.values())
+nome = st.selectbox('Escolha o seu pokemom:', nomes_pokemons.values())
 
 url = f'https://pokeapi.co/api/v2/pokemon/{nome}'
+
 dados_pokemon = requests.get(url).json()
 
 col1, col2, col3 = st.columns(3)
@@ -19,33 +21,33 @@ altura = dados_pokemon['height'] /10
 imc = round(peso / (altura ** 2))
 
 with col1:
-    st.image(dados_pokemon['sprites']['front_default'], width=400)
-    st.write('Normal')
+    st.image(dados_pokemon['sprites']['front_default'], width=550)
 
 with col2:
     st.audio(dados_pokemon['cries']['latest'])
     st.audio(dados_pokemon['cries']['legacy'])
 
 with col3:
-    st.image(dados_pokemon['sprites']['front_shiny'], width=400)
+    st.image(dados_pokemon['sprites']['front_shiny'], width=550)
     st.write('Shiny')
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 =st.columns(3)
+
 with col1:
-    st.metric('Altura', f'{altura} M')
+    st.metric(f'O peso do pokemom é:', peso)
 
 with col2:
-    st.metric('IMC', imc)
+    st.metric(f'A altura do pokemom é:', altura)
 
 with col3:
-    st.metric('Peso', f'{peso} KG')
+    st.metric(f'O IMC é:', imc)
 
-
-tipos, status, locais, habilidades = st.tabs(['Tipos', 'Status', 'Locais', 'Habilidades'])
+status,tipos,habilidades,locais = st.tabs(['status','tipos','habilidades','locais'])
 
 with tipos:
     for i in dados_pokemon['types']:
         st.markdown(f'- {i['type']['name']}')
+
 
 with status:
     hp, ataque, defesa, ataque_esp, defesa_esp, velocidade = st.columns(6)
@@ -68,5 +70,7 @@ with locais:
         st.markdown(f'- {local['location_area']['name']}')
 
 with habilidades:
-    for abilidade in dados_pokemon['abilities']:
-        st.markdown(f'- {abilidade['ability']['name']}')
+    for habilidade in dados_pokemon['abilities']:
+        st.markdown(f'- {habilidade['ability']['name']}')
+
+
